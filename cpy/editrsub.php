c38d21ce76c6e3bae104c9b5f0c44d40d6dd1892
Mazida <141552184+mazilena@users.noreply.github.com>
2025-07-09 19:42:54 +0530

    Delete cpy directory

--- cpy/editrsub.php
+++
@@ -1,7 +0,0 @@
-<?php
-// submit_content.php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $content = $_POST['editor1'];
-    // Process and store content as needed
-    echo "Content received: " . htmlspecialchars($content);
-}
